da4ad97a06ce7d9dacd14bd80aeb25fbaaacb9e1
M-Mafeni <mm17291@my.bristol.ac.uk>
2019-02-27 19:34:24 +0000

    finished working on slicing functions

--- practice/pizza.py
+++ practice/pizza.py
@@ -6,8 +6,10 @@ pizzaMat = []
 f = open(filename,"r")
 #first row
 vals = str.split(f.readline().strip()," ")
-L = vals[2]
-H = vals[3]
+R = int(vals[0])
+C = int(vals[1])
+L = int(vals[2])
+H = int(vals[3])
 print(vals)
 print(L)
 print(H)
@@ -15,3 +17,31 @@ for x in f:
     line = list(x.strip())
     pizzaMat.append(line)
 print(pizzaMat)
+pizza = np.array(pizzaMat)
+print(pizza.transpose())
+results = []
+slices = 0
+endI = 0
+endJ = 0
+while(endI != intR - 1):
+    print()
+#return coordinates of slice
+def getRowSlice(pizza,startI,startJ):
+    sliceCount = 0
+    tomatoCount = 0
+    mushroomCount = 0
+    for i in range(startI,len(pizza)):
+        for j in range(startJ,len(pizza[0])):
+            sliceCount += 1
+            if(pizza[i][j] == 'T'):
+                tomatoCount += 1
+            if(pizza[i][j] == 'M'):
+                mushroomCount += 1
+            if(sliceCount > H and (tomatoCount < L or mushroomCount < L)):
+                return None
+            if(tomatoCount >= L and mushroomCount >= L):
+                if(sliceCount == H):
+                    return i,j
+def getColSlice(pizza,startI,startJ):
+    i,j = getRowSlice(pizza.transpose(),startJ,startI)
+    return j,i
