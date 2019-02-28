639014f2befddc0a25dd4ce2d62590e888490842
Ainesh Sevellaraja <ainesh1998@outlook.com>
2019-02-28 18:31:44 +0000

    loading file added

--- hashcode2019/slideshow.py
+++ hashcode2019/slideshow.py
@@ -1,2 +1,16 @@
 import numpy as np
 import sys
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    tags = []
+    f = open(filename,"r")
+
+    #first row
+    photoCount = int(f.readline())
+
+    #rest
+    for x in range(photoCount):
+        line = str.split(f.readline().strip()," ")
+        tags.append(line)
+    f.close()
