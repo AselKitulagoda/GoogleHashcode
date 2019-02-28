5c97d5237f2859baa84bf68c36055edf01d16793
M-Mafeni <mm17291@my.bristol.ac.uk>
2019-02-28 21:15:26 +0000

    a solution found

--- hashcode2019/slideshow.py
+++ hashcode2019/slideshow.py
@@ -29,7 +29,7 @@ if len(sys.argv) == 2:
             slide = Slide(photo1,i,photo2,i+1)
             i += 1
         slides.append(slide)
-        print(slide)
+     #   print(slide)
         i += 1
     dict_tags = {}
     for s in slides:
@@ -39,7 +39,7 @@ if len(sys.argv) == 2:
         for id in ids:
             key += str(id) + " "
         dict_tags[key] = set(tags)
-    print(dict_tags)
+   # print(dict_tags)
     # for s in slides:
     #     tags = s.tags
     #     for tag in tags:
@@ -55,7 +55,7 @@ if len(sys.argv) == 2:
         i_key = x + "->" + y
         i_common = dict_tags[x] & dict_tags[y]
         intersections[i_key] = i_common 
-    print(intersections)
+    #print(intersections)
 
 
     differencesA = {}
@@ -63,14 +63,14 @@ if len(sys.argv) == 2:
         d_key = x + "->" + y
         d_diff = dict_tags[x] - dict_tags[y]
         differencesA[d_key] = d_diff
-    print(differencesA)
+   # print(differencesA)
 
     differencesB = {}
     for x,y in itertools.combinations(dict_tags,2):
         d_key = y + "->" + x
         d_diff = dict_tags[y] - dict_tags[x]
         differencesB[d_key] = d_diff
-    print(differencesB)
+  #  print(differencesB)
 
     interests = {}
     for x,y in itertools.combinations(dict_tags,2):
@@ -81,5 +81,46 @@ if len(sys.argv) == 2:
         sizeC = len(differencesB[j_key])
         interest = np.amin([sizeA,sizeB,sizeC])
         interests[i_key] = interest
-    print(interests)
+        interests[j_key] = interest
+    #print(interests)
+    results = []
+     #find max
+    max1 = 0
+    initResult = ""
+    sourceResult = ""
+    for key in interests.keys():
+        p = key.split("->")
+        interest = interests[key]
+        if(interest > max1):
+            max1 = interest
+            initResult = p[0]
+            sourceResult = p[1]
+    #print(initResult)
+    results.append(initResult)
+    results.append(sourceResult)
+    i = 0
+    while(i < len(slides) - 2):
+        resultI = ""
+        max2 = 0
+        for key in interests.keys():
+            p = key.split("->")
+            if(sourceResult == p[0]):
+                interest = interests[key]
+                if(interest > max2):
+                    #check that sourceResult isn't in results
+                    d = p[1]
+                    if d not in results:
+                        max2 = interest
+                        sourceResult = p[1]
+
+        results.append(sourceResult)
+        i += 1
+#print(results)
+#output path
+f = open("outputB.txt","w+")
+f.write(str(len(slides)) + "\n")
+for result in results:
+    result = result.strip()
+    f.write(result + "\n")
+
         
